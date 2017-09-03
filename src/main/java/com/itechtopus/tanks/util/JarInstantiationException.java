d429fc183fc978a8fb536fa0ecd8a3572253bfc4
PaintballWorld <PaintballWorld@rambler.ru>
2017-09-03 17:49:14 +0600

    Added new type of Exception to manage errors during processing jars

---
+++ src/main/java/com/itechtopus/tanks/util/JarInstantiationException.java
@@ -0,0 +1,16 @@
+package com.itechtopus.tanks.util;
+
+/**
+ * @author Yerlan Akzhanov
+ * @link GitHub : github.com/Paintballworld
+ * @link personal : www.itechtopus.com
+ * Paintballworld@rambler.ru
+ * date: 03.09.17
+ * GameEngine
+ */
+public class JarInstantiationException extends RuntimeException {
+
+    public JarInstantiationException(String message) {
+        super(message);
+    }
+}
