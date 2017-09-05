2a012805d9efb2634d8106485fb7189465e10c21
PaintballWorld <PaintballWorld@rambler.ru>
2017-09-05 21:45:56 +0600

    Implemented mock drawing a tank on local window

--- src/main/java/com/itechtopus/tanks/visualisation/impl/j_frame/Surface.java
+++ src/main/java/com/itechtopus/tanks/visualisation/impl/j_frame/Surface.java
@@ -3,31 +3,47 @@ package com.itechtopus.tanks.visualisation.impl.j_frame;
 import com.itechtopus.tanks.implementations.PositionReal;
 import com.itechtopus.tanks.interfaces.field.Field;
 import com.itechtopus.tanks.interfaces.models.MovingModel;
-import com.itechtopus.tanks.model.ModelType;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 import java.util.*;
-import javax.swing.JPanel;
+import java.util.List;
+import javax.swing.*;
 import javax.swing.Timer;
 
 class Surface extends JPanel implements ActionListener {
 
     private final int DELAY = 100;
     private Timer timer;
-    Field field;
-    List<MovingModel> models = new ArrayList<>();
+    private Field field;
+    private List<MovingModel> models = new ArrayList<>();
     private final int BLOCK_SIZE;
+    private Image tank;
+    private static final String IMAGE_LOCATION = "com/itechtopus/tanks/visualization/imp/j_frame/images/";
+    private static final String TANK_IMAGE_LOCATION = IMAGE_LOCATION + /*"boom.png";//*/"gold_tank.png";
+    private BufferedImage bufTank;
 
     public Surface(int blockSize, Field field, List<MovingModel> models) {
         this.field = field;
         this.models = models;
         BLOCK_SIZE = blockSize;
+        initTextures();
         initTimer();
+    }
+
+    private void initTextures() {
+        URL url = Surface.class.getClassLoader().getResource(TANK_IMAGE_LOCATION);
+        tank = new ImageIcon(url).getImage();
+        bufTank = new BufferedImage(4 * BLOCK_SIZE, 4 * BLOCK_SIZE, BufferedImage.TYPE_INT_RGB);
+        Graphics2D gt = bufTank.createGraphics();
+        gt.drawImage(tank, 0, 0, 4 * BLOCK_SIZE, 4 * BLOCK_SIZE, null);
+        AlphaComposite ac = java.awt.AlphaComposite.getInstance(AlphaComposite.CLEAR, 0.5F);
+        gt.setComposite(ac);
     }
 
     private void initTimer() {
@@ -47,26 +63,24 @@ class Surface extends JPanel implements ActionListener {
 
         for (MovingModel model : models) {
             PositionReal p = (PositionReal) model.getPosition();
-            g2d.setPaint(Color.RED);
-            g2d.fill(new Rectangle2D.Double(
-                    p.getRealMinX() * BLOCK_SIZE,
-                    p.getRealMinY() * BLOCK_SIZE,
-                    (p.getRealMaxX() - p.getRealMinX()) * BLOCK_SIZE,
-                    (p.getRealMaxY() - p.getRealMinY()) * BLOCK_SIZE));
+            g2d.setPaint(Color.BLACK);
+            g2d.fillRect(0, 0, field.getWidth() * BLOCK_SIZE, field.getHeight() * BLOCK_SIZE);
+
+            double rotationRequired = Math.toRadians(((model.getDirection().ordinal() - 1) % 4)* 90);
+            AffineTransform transform = new AffineTransform();
+            transform.rotate(rotationRequired, bufTank.getWidth(this)/2, bufTank.getHeight(this)/2);
+            AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+            tank = op.filter(bufTank, null);
+
+            g2d.drawImage(tank, (int) (p.getRealMinX() * BLOCK_SIZE),
+                    (int) (p.getRealMinY() * BLOCK_SIZE),
+                    (int) ((p.getRealMaxX() - p.getRealMinX()) * BLOCK_SIZE),
+                    (int) ((p.getRealMaxY() - p.getRealMinY()) * BLOCK_SIZE), null);
         }
-
-
-        /*Random r = new Random();
-
-        for (int i = 0; i < 2000; i++) {
-
-            int x = Math.abs(r.nextInt()) % w;
-            int y = Math.abs(r.nextInt()) % h;
-            g2d.drawLine(x, y, x, y);
-        }*/
     }
 
     private void drawCells(Graphics2D g2d) {
+        g2d.setColor(Color.WHITE);
         for (int y = 0; y < field.getHeight(); y++) {
             g2d.drawLine(0, y * BLOCK_SIZE, field.getWidth() * BLOCK_SIZE, y * BLOCK_SIZE);
         }
