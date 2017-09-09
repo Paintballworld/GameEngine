6a650223011fb1f8627e48735bb94e47ae336c20
PaintballWorld <PaintballWorld@rambler.ru>
2017-09-09 07:31:11 +0600

    Moved BLOCK_SIZE configuration to a static Settings class

---
+++ src/main/java/com/itechtopus/tanks/staticSettings/Settings.java
@@ -0,0 +1,14 @@
+package com.itechtopus.tanks.staticSettings;
+
+/**
+ * @author Yerlan Akzhanov
+ * @link GitHub : github.com/Paintballworld
+ * @link personal : www.itechtopus.com
+ * Paintballworld@rambler.ru
+ * date: 09.09.17
+ * GameEngine
+ */
+public class Settings {
+
+    public static final int BLOCK_SIZE = 12;
+}
